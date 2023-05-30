fc1bab70848a1c0a9fa225ff01f58619bbcf9fc4
Alp Beyazgül <alpbeyazgul@hotmail.com>
2023-05-30 23:22:15 +0300

    Update urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,1 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
+    path('post/create/', views.PostCreateView.as_view(), name='post-create'),
+    path('post/update/<int:pk>/', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/delete/<int:pk>/', views.PostDeleteView.as_view(), name='post-delete'),
+]
 
